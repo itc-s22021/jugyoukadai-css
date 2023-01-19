ad2e472a1faac55080e2c7511c4d04f6dd8ab84d
s22021 <s22021@std.it-college.ac.jp>
2023-01-19 05:00:43 +0000

    2pe-zime

---
+++ pages/times.js
@@ -0,0 +1,13 @@
+import styles from '../styles/times.module.css'
+export default function Home () {
+  return (
+    <div className={styles.box}>
+      <h1 className={styles.h1}>Prep Times</h1>
+      <h2 className={styles.h2}>Headline</h2>
+      <h3 className={styles.h3}>Article 1</h3>
+      <p className={styles.p}>sentence comes here sentence comes here sentence comes here sentence comes here</p>
+      <h3 className={styles.h3}>Article 2</h3>
+      <p className={styles.p}>sentence comes here sentence comes here sentence comes here sentence comes here</p>
+    </div>
+  )
+}
